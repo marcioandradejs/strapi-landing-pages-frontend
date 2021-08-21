00273c0266589c7b359aaf18f1b54e54a78168b6
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-20 21:23:42 -0300

    componente de Heading criado

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -2,11 +2,15 @@ import { createGlobalStyle, css } from 'styled-components';
 import { theme } from './theme';
 
 export const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+  }
+
+  img {
+    max-width: 100%;
   }
 
   html {
@@ -19,7 +23,7 @@ export const GlobalStyles = createGlobalStyle`
     font-family: ${({ theme }) => theme.font.family.default};
   }
 
-  h1 {
+  h1, h2, h3, h4, h5, h6 {
     font-family: ${({ theme }) => theme.font.family.secondary};
   }
 `;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -8,9 +8,24 @@ export const theme = {
     lightBg: '#fff',
     darkBg: '#0A1128',
   },
-  fonts: {
-    default: "'Open Sans', sans-serif",
-    secondary: "'Montserrat', sans-serif",
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '.8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4rem',
+      xzlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
   },
   spacings: {},
 };
