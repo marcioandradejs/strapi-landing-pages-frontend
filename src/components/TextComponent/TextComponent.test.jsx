14e61faa927596437e0fe086e6d2362d5ba744d8
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-22 17:26:37 -0300

    Testes - TextComponent

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -3,8 +3,12 @@ import { renderTheme } from '../../styles/render-theme';
 import { TextComponent } from '.';
 
 describe('<TextComponent />', () => {
-  it('should render', () => {
+  it('should render a text', () => {
     renderTheme(<TextComponent>Children</TextComponent>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByText('Children')).toBeInTheDocument();
+  });
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
