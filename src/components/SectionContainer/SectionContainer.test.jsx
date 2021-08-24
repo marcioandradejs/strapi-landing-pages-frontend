b0cfa6337de2355eff8cbb47340c7761ade4fa0b
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-24 13:42:08 -0300

    Testes componente de container e background

--- src/components/SectionContainer/SectionContainer.test.jsx
+++ src/components/SectionContainer/SectionContainer.test.jsx
@@ -1,20 +1,15 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
 import { SectionContainer } from '.';
 
-export default {
-  title: 'SectionContainer',
-  component: 'SectionContainer',
-  args: {
-    children: 'SectionContainer',
-  },
-  argTypes: {
-    children: { type: 'string' },
-  },
-};
-
-export const Template = (args) => {
-  return (
-    <div>
-      <SectionContainer {...args} />
-    </div>
-  );
-};
+describe('<SectionContainer />', () => {
+  it('should render content', () => {
+    const { container } = renderTheme(
+      <SectionContainer>
+        <h1>Children</h1>
+      </SectionContainer>,
+    );
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
