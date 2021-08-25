d7e1a435474b6c967462ba8fb64a5d08f6e52aab
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-25 10:21:53 -0300

    componente GridImage (Gallery)

--- src/components/GridImage/styles.js
+++ src/components/GridImage/styles.js
@@ -12,29 +12,23 @@ export const Container = styled.div`
 
 export const Grid = styled.div`
   ${({ theme }) => css`
-    counter-reset: grid-counter;
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(28rem, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
     gap: ${theme.spacings.large};
   `}
 `;
 
 export const GridElement = styled.div`
   ${({ theme }) => css`
-    ${HeadingContainer} {
-      position: relative;
-      left: 5rem;
-    }
-    ${HeadingContainer}::before {
-      counter-increment: grid-counter;
-      content: counter(grid-counter);
-      position: absolute;
-      font-size: 8rem;
-      font-style: italic;
-      top: -4rem;
-      left: -5rem;
-      transform: rotate(5deg);
-      color: ${theme.colors.secondaryColor};
+    overflow: hidden;
+  `}
+`;
+
+export const Image = styled.img`
+  ${({ theme }) => css`
+    transition: all 300ms ease-in-out;
+    &:hover {
+      transform: scale(1.2) rotate(10deg);
     }
   `}
 `;
