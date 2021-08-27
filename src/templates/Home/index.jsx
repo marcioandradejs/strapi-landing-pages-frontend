f31d5d612077425026d122ee4b54a7fee2295834
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-27 16:43:20 -0300

    adicionando components na pagina (com dois Undefineds)

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Heading } from '../../components/Heading';
+import { GridTwoColumn } from '../../components/GridTwoColumn';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 import * as Styled from './styles';
 import { mapData } from '../../api/map-data';
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -19,7 +23,6 @@ function Home() {
         const json = await data.json();
         const pageData = mapData(json);
         setData(pageData[0]);
-        console.log(pageData);
       } catch (e) {
         setData(undefined);
       }
@@ -36,7 +39,38 @@ function Home() {
     return <Loading />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+        console.log(component);
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumn key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 }
 
 export default Home;
